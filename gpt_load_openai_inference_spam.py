bc7daa5e4e22bb8f5c99b775e46e0a5bfde4eb5a
pavelkrolevets <pavelkrolevets@gmail.com>
2025-12-09 12:46:11 +1000

    add spam classifier

--- gpt_load_openai_inference_spam.py
+++ gpt_load_openai_inference_spam.py
@@ -250,113 +250,31 @@ def calc_accuracy_loader(data_loader, model, device, num_batches=None):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 torch.manual_seed(123)
-train_accuracy = calc_accuracy_loader(train_loader, model, device, num_batches=10)
-val_accuracy = calc_accuracy_loader(val_loader, model, device, num_batches=10)
-test_accuracy = calc_accuracy_loader(test_loader, model, device, num_batches=10)
-print(f"Training accuracy: {train_accuracy*100:.2f}%")
-print(f"Validation accuracy: {val_accuracy*100:.2f}%")
-print(f"Test accuracy: {test_accuracy*100:.2f}%")
 
+model_state_dict = torch.load("review_classifier.pth, map_location=device")
+model.load_state_dict(model_state_dict)
 
-def calc_loss_batch(input_batch, target_batch, model, device):
-    input_batch = input_batch.to(device)
-    target_batch = target_batch.to(device)
-    logits = model(input_batch)[:, -1, :]
-    loss = torch.nn.functional.cross_entropy(logits, target_batch)
-    return loss
-
-
-def calc_loss_loader(data_loader, model, device, num_batches=None):
-    total_loss = 0.0
-    if len(data_loader) == 0:
-        return float("nan")
-    elif num_batches is None:
-        num_batches = len(data_loader)
-    else:
-        num_batches = min(num_batches, len(data_loader))
-    for i, (input_batch, target_batch) in enumerate(data_loader):
-        if i < num_batches:
-            loss = calc_loss_batch(input_batch, target_batch, model, device)
-            total_loss += loss.item()
-        else:
-            break
-    return total_loss / num_batches
-
-
-with torch.no_grad():
-    train_loss = calc_loss_loader(train_loader, model, device, num_batches=5)
-    val_loss = calc_loss_loader(val_loader, model, device, num_batches=5)
-    test_loss = calc_loss_loader(test_loader, model, device, num_batches=5)
-print(f"Training loss: {train_loss:.3f}")
-print(f"Validation loss: {val_loss:.3f}")
-print(f"Test loss: {test_loss:.3f}")
-
-
-def train_classifier_simple(
-    model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter
+def classify_review(
+    text, model, tokenizer, device, max_length=None, pad_token_id=50256
 ):
-    train_losses, val_losses, train_accs, val_accs = [], [], [], []
-    examples_seen, global_step = 0, -1
-    for epoch in range(num_epochs):
-        model.train()
-        for input_batch, target_batch in train_loader:
-            optimizer.zero_grad()
-            loss = calc_loss_batch(input_batch, target_batch, model, device)
-            loss.backward()
-            optimizer.step()
-            examples_seen += input_batch.shape[0]
-            global_step += 1
-            if global_step % eval_freq == 0:
-                train_loss, val_loss = evaluate_model(
-                    model, train_loader, val_loader, device, eval_iter
-                )
-                train_losses.append(train_loss)
-                val_losses.append(val_loss)
-                print(
-                    f"Ep {epoch+1} (Step {global_step:06d}): "
-                    f"Train loss {train_loss:.3f}, "
-                    f"Val loss {val_loss:.3f}"
-                )
-        train_accuracy = calc_accuracy_loader(
-            train_loader, model, device, num_batches=eval_iter
-        )
-        val_accuracy = calc_accuracy_loader(
-            val_loader, model, device, num_batches=eval_iter
-        )
-        print(f"Training accuracy: {train_accuracy*100:.2f}% | ", end="")
-        print(f"Validation accuracy: {val_accuracy*100:.2f}%")
-        train_accs.append(train_accuracy)
-        val_accs.append(val_accuracy)
-
-
-def evaluate_model(model, train_loader, val_loader, device, eval_iter):
     model.eval()
+    input_ids = tokenizer.encode(text)
+    supported_context_length = model.pos_emb.weight.shape[1]
+    input_ids = input_ids[: min(max_length, supported_context_length)]
+    input_ids += [pad_token_id] * (max_length - len(input_ids))
+    input_tensor = torch.tensor(input_ids, device=device).unsqueeze(0)
     with torch.no_grad():
-        train_loss = calc_loss_loader(
-            train_loader, model, device, num_batches=eval_iter
-        )
-        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
-    model.train()
-    return train_loss, val_loss
+        logits = model(input_tensor)[:, -1, :]
+    predicted_label = torch.argmax(logits, dim=-1).item()
+    return "spam" if predicted_label == 1 else "not spam"
 
 
-import time
-
-start_time = time.time()
-torch.manual_seed(123)
-optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5, weight_decay=0.1)
-num_epochs = 5
-train_losses, val_losses, train_accs, val_accs, examples_seen = train_classifier_simple(
-    model,
-    train_loader,
-    val_loader,
-    optimizer,
-    device,
-    num_epochs=num_epochs,
-    eval_freq=50,
-    eval_iter=5,
+text_1 = (
+    "You are a winner you have been specially"
+    " selected to receive $1000 cash or a $2000 award."
 )
-end_time = time.time()
-execution_time_minutes = (end_time - start_time) / 60
-print(f"Training completed in {execution_time_minutes:.2f} minutes.")
-torch.save(model.state_dict(), "review_classifier.pth")
+print(
+    classify_review(
+        text_1, model, tokenizer, device, max_length=train_dataset.max_length
+    )
+)
--- gpt_model.py
+++ gpt_model.py
@@ -337,26 +337,3 @@ def load_weights_into_gpt(gpt, params):
     gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
     gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
     gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
-
-
-def calc_accuracy_loader_spam(data_loader, model, device, num_batches=None):
-    model.eval()
-    correct_predictions, num_examples = 0, 0
-
-    if num_batches is None:
-        num_batches = len(data_loader)
-    else:
-        num_batches = min(num_batches, len(data_loader))
-    for i, (input_batch, target_batch) in enumerate(data_loader):
-        if i < num_batches:
-            input_batch, target_batch = input_batch.to(device), target_batch.to(device)
-
-            with torch.no_grad():
-                logits = model(input_batch)[:, -1, :]  # Logits of last output token
-            predicted_labels = torch.argmax(logits, dim=-1)
-
-            num_examples += predicted_labels.shape[0]
-            correct_predictions += (predicted_labels == target_batch).sum().item()
-        else:
-            break
-    return correct_predictions / num_examples
